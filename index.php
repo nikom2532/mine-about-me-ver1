4aea191758119a6e7b3a602f9e3d8eac00f01db3
nikom2532 <nikom2532@hotmail.com>
2013-06-11 03:01:56 +0700

    Add my web

---
+++ index.php
@@ -0,0 +1,359 @@
+<?php // <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"> ?>
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+	<TITLE> About us </TITLE>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8;" />
+	<META NAME="Generator" CONTENT="Editor">
+	<META NAME="Author" CONTENT="">
+	<META NAME="Keywords" CONTENT="">
+	<META NAME="Description" CONTENT="">
+	<STYLE type=text/css>
+		A:link {
+			COLOR: #0088ff;
+			text-decoration: none;
+			text-shadow: 5px 5px 20px #0088ff;
+		}
+		A:visited {
+			COLOR: #0088ff;
+			text-decoration: none;
+			text-shadow: 5px 5px 20px #0088ff;
+		}
+		A:hover {
+			COLOR: #FF0000;
+			text-decoration: underline;
+			text-shadow: 5px 5px 20px #FF0000;
+		}
+	
+		/*
+		.s1{
+			width: 34%; 
+			top: 0px;
+			text-align: right; 
+			vertical-align: middle;
+			border-style:solid;
+			border-color:#98bf21;
+		}
+		.s2{
+			width: 33%; 
+			top: 0px;
+			left: 34%;
+			height: 1000px;
+			top: 0px;
+			vertical-align: middle;
+			border-style:solid;
+			border-color:#98bf21;
+		}
+		.s3{
+
+			width: 33%; 
+			top: 0px;
+			left: 33%
+			text-align: left; 
+			vertical-align: middle;
+			border-style:solid;
+			border-color:#98bf21;
+
+		}	
+		.s4{
+			top: 1000px;
+		}
+		*/
+/*
+position:fixed;
+*/
+/*
+display: block;
+*/
+		.s1{
+		    position: absolute;
+			width: 450px; 
+			top: 10px;
+			left: 60px;
+			text-align: right; 
+			vertical-align: middle;
+			
+			border: 1px solid;
+			border-radius: 25px;
+			-webkit-border-radius:25px; 
+			-moz-border-radius:25px;
+			-o-border-radius:25px; 
+			border-style:solid;
+			border-color:#98bf21;
+			box-shadow: 5px 5px 5px #888888;
+			-webkit-box-shadow: 5px 5px 5px #888888;
+			-moz-box-shadow: 5px 5px 5px #888888;
+			-o-box-shadow: 5px 5px 5px #888888;
+		}
+		.s2{
+		    position: absolute;
+			width: 250px; 
+			top: 10px;
+			left: 540px;
+			vertical-align: middle;
+			/*
+			margin-left: 10px;
+			margin-right: 10px;
+			*/
+			
+			border: 1px solid;
+			border-radius: 25px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px; 
+			-o-border-radius: 25px;
+			border-style: solid;
+			border-color: #98bf21;
+			box-shadow: 5px 5px 5px #888888;
+			-webkit-box-shadow: 5px 5px 5px #888888;
+			-moz-box-shadow: 5px 5px 5px #888888;
+			-o-box-shadow: 5px 5px 5px #888888;
+		}
+		.s3{
+		    position: absolute;
+			width: 380px; 
+			top: 10px;
+			left: 805px;
+			/*right: 10px;*/
+			text-align: left; 
+			vertical-align: middle;
+			
+			border: 1px solid;
+			border-radius: 25px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			-o-border-radius: 25px;
+			border-style: solid;
+			border-color: #98bf21;
+			box-shadow: 5px 5px 5px #888888;
+			-webkit-box-shadow: 5px 5px 5px #888888;
+			-moz-box-shadow: 5px 5px 5px #888888;
+			-o-box-shadow: 5px 5px 5px #888888;
+		}	
+		.s4{
+		    position: absolute;
+			width: 450px; 
+		    top: 880px;
+			left: 750px;
+		    text-align: right;
+   			vertical-align: middle;
+			text-shadow: 5px 5px 5px #685b52;
+		    
+		    border:1px solid;
+			border-radius:25px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			-o-border-radius: 25px;
+			border-style: solid;
+			/*border-color: #ddaa89;*/
+			border-color: #303030;
+			box-shadow: 5px 5px 5px #888888;
+			-webkit-box-shadow: 5px 5px 5px #888888;
+			-moz-box-shadow: 5px 5px 5px #888888;
+			-o-box-shadow: 5px 5px 5px #888888;
+		}
+		.s1, .s2, .s3, .s4{
+			transform:rotate(5deg);
+			-ms-transform:rotate(5deg);
+			-moz-transform:rotate(5deg);
+			-webkit-transform:rotate(5deg);
+			-o-transform:rotate(5deg);
+			/*
+			transform:skew(-5deg,-5deg);
+			-ms-transform:skew(-5deg,-5deg);
+			-moz-transform:skew(-5deg,-5deg); 
+			-webkit-transform:skew(-5deg,-5deg); 
+			-o-transform:skew(-5deg,-5deg); 
+			*/
+			
+			/* transition:width 2s, height 2s, rotate 2s; */
+			transition: all 1s;
+			-moz-transition: all 1s;
+			-webkit-transition: all 1s;
+			-o-transition:width all 1s;
+		}
+		.s1, .s3{
+			color: #ffffff;
+			font-size: 17px;
+			text-shadow: 5px 5px 5px #101010;
+			word-wrap:break-word;
+		}
+		.s2{
+			color: #ffffff;
+			font-size: 17px;
+			text-shadow: 5px 5px 5px #101010;
+			word-wrap:break-word;
+		}
+		.s1:hover, .s2:hover, .s3:hover{
+			color: #ffffff;
+			font-size: 17px;
+			text-shadow: 5px 5px 5px #101010;
+			word-wrap:break-word;
+		}
+		.s1:hover{
+			background-color:#333333;
+			transform: translate(0px,50px) scale(1.1,1.1);
+			-moz-transform: translate(0px,50px) scale(1.1,1.1); 
+			-webkit-transform: translate(0px,50px) scale(1.1,1.1); 
+			-o-transform: translate(0px,50px) scale(1.1,1.1);
+			
+			width: 520px;
+			
+			z-index: 10000;
+		}
+		.s2:hover{
+			background-color:#333333;
+			transform:rotate(-5deg) translate(-20px,-10px);
+			-moz-transform:rotate(-5deg) translate(-20px,-10px); 
+			-webkit-transform: translate(-20px,-10px); 
+			-o-transform:rotate(-5deg) translate(-20px,-10px); 
+			z-index: 10000;
+		}
+		.s3:hover{
+			background-color:#333333;
+			transform: translate(20px,0px);
+			-moz-transform: translate(20px,0px);
+			-webkit-transform: translate(20px,0px);
+			-o-transform: translate(20px,0px);
+			
+			z-index: 10000;
+		}
+		.s4:hover{
+			background-color:#333333;
+			transform: translate(-50px,-10px);
+			-moz-transform: translate(-50px,-10px);
+			-webkit-transform: translate(-50px,-10px);
+			-o-transform: translate(-50px,-10px);
+			
+			z-index: 10000;
+		}
+	</style>
+</HEAD>
+<BODY bgcolor="#333333">
+	<span class="s1">
+		<p>
+		My names are
+		Armíng Húang
+		(Mr. Nikom Suwankamol)
+		光明 (阿明),
+		노랑황의,
+		グアン,
+		หมิง นิคม,
+		-Мин Хуан,
+		زردنورمینگ,
+		גואַנג-מינג הואַנג,
+		قوانغ مينغ هوانغ
+		<br>
+		<br>i am studying B.Sc.Computer Science(Inter) &#64;King Mongkut's Institute of Technology Ladkrabang (KMITL)
+		<br>im interesting : C, C++, OpenGL, Ruby, Java, MATLAB, Web Development: PHP, MySQL, javascript, jQuery, AJAX, NoSQL:{Cassandra, MongoDB}, Android SDK, Pascal, Turtle Logo,
+		<br>I used to use Macromedia Flash 5, Adobe Photoshop {5.5, 6, 7, CS2}, Adobe Dreamweaver 3, Gimp, Inkscape vector, AutoDisk Maya 2011, AutoDisk 3D Studio Max, GameCore, Game Maker 5, Microsoft Office 97, Ashampoo UnInstaller, BitComet, Download Accelerator Plus, Flashget, Dev Cpp, Daemon Tools, Easy CD-DA Extractor 12, FileZilla, Mozilla Firefox, Google Chrome, Norton AntiVirus, Nero Burn Rom 5.5+, PowerISO, PowerQuest Partition Magic, PowerQuest Boot Manager, Netbean, Eclipse, WinRAR, Winzip, 7-zip, Audacity 1.2, Avira AntiVir, Dia, Camstudio PC, HyperSnap 6, jGRASP, MagicISO, WinAVI Video Converter Software
+		<br>I used to use Microsoft Dos {6.10 and after}, PC-DOS (IBM), OS/2, Microsoft Windows {3.x, 95, 98, 98 Second Edition, NT4, 2000, ME, XP (& SP1, 2, 3), Seven}, Linux {Chrome OS Beta, Debien 10+, Ubuntu (6.04, 6.10, 10.04, 10.10 Desktop, 11.10 server), Redhat(version in 2004 and/or below), TLE(ทะเล) 4,non-Shockware!}, Unix{FreeBSD 8, Solaris x86, }, NeXTSTEP, Mac OS X Lion, MorphOS version 2.4, BeOS Base {Haiku R1 Alpha 2 }, Plan 9, Android {2.2.1, 2.2.3, 3.0.1}, iOS{4, 5} Operating Systems
+
+		<br><br>I like to play Basketball, watch Korean & Taiwan & Thai TV-Series, listen Musics.
+		<Br>
+		I was Born in Bangkok, Thailand.
+		I am studied in La Salle Collage Bangkok during 1994-2007 on 2th Kindergarten to Grade 12
+		next continue studying in B.Sc. Computer Science in King Mongkut's Institute of Technology Ladkrabang (KMITL), Thailand
+		I like to play Basketball, Batminton, Tabletennis.
+		I like to watch on TV-Asian Series(Thai Korean Taiwan).
+		I like to play Travian, Counter-Strike, Age of Empires II, Rise of Nation, Age of Empires Online.
+		I am lazy to read very long books. I hate to exams on school or university.
+		I like to surf the Internet.
+		I like Thai food.
+		I like to speed Thai Language. I like to listen Pop musics. (English, Chinese, Canton, Thai, Korean, Japan, Vietnum, Indian, Persian, Russian, French, Spanish, Filipino) *** ^_^
+		</p>
+	</span>
+	<span class="s2">
+		<center>
+		<br><img src="./Image/SSL10858[4-2].JPG" ALT="iMing" width="200" height="177.92869269949066213921901528014" BORDER="0">
+		<p><a href="#" onClick="document.getElementById('aboutMe').innerHTML = 'This is about me page'; ">About me</a></p>
+		<div id="aboutMe" style="text-align:center; color: 00bbee"></div>
+		<p><a href="http://blog.nikom2532.co.cc">Blog</a></p>
+		<?php /*
+		<br>
+		<br><a href="../social">nikom2532</a>
+		
+		./Job_Resume2012/Nikom_resume4_1.pdf
+		
+		*/
+		?>
+		<p><a href="https://docs.google.com/open?id=0B8zOenNDooXXZzUtdzVqbGcyY1E">Resume</a> <font color="ff1111">new!</font></p>
+		<p>
+			<a href="../_other/v2005/Index.htm">v2003 designed</a>
+			<br/ >open with Microsoft Internet Explorer Browser version 5.5 or below!
+		</p>
+		<p>
+			<a href="../_other/RSU/WebDesign/Index.htm">RSU designed contest</a>
+			<br/ >open with IE 5.5 or below!
+		</p>
+		<p>
+			<a href="../_other/Mo6/cpp/Index.htm">C++</a>
+			<br/ >open with IE 5.0 or below!
+		</p>
+		<p>
+			<a href="../_other/Mo6/Aubsumchum/Index.htm">King Rama9</a>
+			<br/ >open with IE 5.0 or below! + Flash 5 player or below!!! insite the web, do not click "Back".
+		</p>
+		<p>
+			<a href="../_other/Mo6/Bay_Site/index.php">Bay Joke reader</a>
+			<br/ >open with IE 5.0 or below!
+		</p>
+		</center>
+	</span>
+	<span class="s3">
+		<br>====================
+		<br>My Channels' Networking
+		<br>====================
+		<br>
+		<br><a href="mailto:nikom2532@hotmail.com">Windows Live Hotmail</a> : nikom2532@hotmail.com
+		<br><a href="https://www.ibm.com/developerworks/mydeveloperworks/profiles/user/ArmingHuang">IBM Developer works</a>
+		<br><a href="http://twitter.com/nikom2532">twitter: @nikom2532</a>
+		<br><a href="https://plus.google.com/115185082420374327964/">Google plus</a>
+		<br><a href="http://alturl.com/93nvt">Facebook</a>
+		<br><a href="http://wwww.weibo.com/nikom2532/">SINA Weibo(新浪微博)</a>
+		<br><a href="http://www.skype.com">Skype name</a> : nikom2532
+		<br><a href="http://www.whatsapp.com">Whatapp</a> : +66 8 5506 5953
+		<br><a href="http://www.viber.com">Viber</a> : (+66)855065953
+		<br><a href="http://line.naver.jp/en/">Line (ライン)</a> : +66 8 5506 5953
+		<br><a href="http://weixin.qq.com">WeiXin (微信)</a> : +66 8 5506 5953
+		<br><a href="http://www.imqq.com/">Tencent QQ</a> : 2658406838
+		<br><a href="mailto:nikom2532@qq.com">QQ 邮箱</a> : nikom2532@qq.com
+		<br><a href="mailto:nikom2532@yahoo.com">Yahoo Mail</a>: nikom2532@yahoo.com
+		<br><a href="mailto:nikom2532@gmail.com">Gmail</a> : nikom2532@gmail.com
+		<br><a href="http://www.renren.com/profile.do?id=383146032">renren (人人网)</a>
+		<br><a href="http://soundcloud.com/nikom2532">SoundCloud</a>
+		<br><a href="http://t.qq.com/nikom2532">tweet.qq</a>
+		<br><a href="http://instagr.am">Instagram</a> : nikom2532
+		<br><a href="http://www.linkedin.com/in/nikom2532">LinkedIn</a>
+		<br><a href="http://www.hi5.com/friend/profile/displayProfile.do?userid=226392149">Hi5</a>
+		<br><a href="http://pulse.yahoo.com/nikom2532">YAHOO! PULSE</a>
+		<br><a href="http://www.flickr.com/photos/nikom2532">Flickr
+		<br><a href="http://www.youtube.com/nikom2532">Youtube Channel</a>
+		<br><a href="http://picplz.com/user/nikom2532/">Picplz</a>
+		<br><a href="http://friendfeed.com/nikom2532">friendfeed</a>
+		<br><a href="http://voxer.com">Voxer</a> : arming.hang.fb.1289836787
+		<br><a href="http://www.blogger.com/profile/02508875033021847292">Blogger</a>
+		<br><a href="https://foursquare.com">Foursquare</a> : nikom2532
+		<br><a href="http://www.spriiing.com">Spriiing</a> : 100BSS11 <font color="#ff0000">[not avalable right now]</font>
+		<br><a href="http://www.tango.me">Tango</a> : (+66)855065953 <font color="#ff0000">[not avalable right now]</font>
+		<br><a href="http://www.fring.com/">Fring</a> : (+66)855065953 <font color="#fc0500">[few times]</font>
+		<br><a href="https://www.facebook.com/channel.v.bangkok.music">Channel [V] Bangkok Music</a>
+		<p></p>
+	</span>
+
+
+	<P class="s4">
+		<font color="#ddaa89">
+			<br/>
+			Page Viewed : 
+			<?php
+			include("./Counter/index.php"); 
+			?> 
+			<br>
+			Copyright <font size="4">&#169;</font> since October 17th, 2011 &#64; 10:17 AM (GMT)
+			<Br>
+			by Arm&#237;ng H&#250;ang (Nikom Suwankamol). Alright reserved <font size="4">&#174;</font>
+		</font>
+		<p></p>
+	</P>
+</BODY>
+</HTML>
---
+++ index3.php
@@ -0,0 +1,141 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+ <HEAD>
+  <TITLE> About us </TITLE>
+  <meta http-equiv=”Content-Type” content=”text /html; charset=UTF-8” />
+  <META NAME="Generator" CONTENT="Editor">
+  <META NAME="Author" CONTENT="">
+  <META NAME="Keywords" CONTENT="">
+  <META NAME="Description" CONTENT="">
+	<STYLE type=text/css>
+	A:link {
+		COLOR: #0088ff;
+		text-decoration: blink;
+	}
+	A:visited {
+		COLOR: #0088ff;
+		text-decoration: blink;
+	}
+	A:hover {
+		COLOR: #FF0000;  ;text-decoration: underline
+	}
+</style>
+
+ </HEAD>
+
+ <BODY bgcolor="#333333">
+
+   <table border="0" width="100%" cellpadding="2" cellspacing="2">
+    <tr cellpadding="2" cellspacing="2">
+	 <td align="right" valign="middle" style="width: 500px;" cellpadding="2" cellspacing="2">
+	  <font size="4" color="#ffffff">
+		<br>My names are
+		<br>Nikom Suwankamol, 
+		<br>Arming Huang,
+		<br>黃光明 (阿明),
+		<br>노랑황의,
+		<br>グアン,
+		<br>หมิง นิคม,
+		<br>Гуан-Мин Хуан,
+		<br>زردنورمینگ,
+		<br>גואַנג-מינג הואַנג,
+		<br>قوانغ مينغ هوانغ
+		<br>
+		<br>i am studying B.Sc.Computer Science(Inter) @KMITL
+		<br>im interesting : C++,OpenGL,Java,MATLAB,Web Development:PHP,MySQL,js,AJAX,JSF,NoSQL:Cassandra, Basketball,Korean&Taiwan&Thai TV-Series
+		<br>
+		<br>I was Born in Bangkok, Thailand
+		<br>Studied in La Salle Collage Bangkok during 1994-2007
+		<br>on 2th Kindergarten to Grade 12
+		<br>next continue studying in B.Sc. Computer Science
+		<br>in King Mongkut's Institute of Technology Ladkrabang (KMITL), Thailand
+		<br>I like to play Basketball, Batminton, Tabletennis
+		<br>I like to watch on TV-Asian Series(Thai Korean Taiwan) *** ^_^
+		<br>I like to play Travian, Counter-Strike, Age of Empires II, Rise of Nation, Age of Empires Online
+		<br>I am lazy to read very long books
+		<br>I hate to exams on school or university
+		<br>I like to surf the Internet
+		<br>I like Thai food
+		<br>I like to speed Thai Language
+		<br>I like to listen Pop musics
+		<br>(English Chinese Canton Thai Korean Japan Vietnum  Indian Persian Russian French Spanish)
+		<br>
+		<br>
+	  </font>
+	 </td>
+	 <td valign="middle" style="width: 250px; height: 200px;" cellpadding="2" cellspacing="2">
+	  <font size="4" color="#ffffff">
+	   <br><center>
+	   <br><img src="./Image/SSL10858[4-2].JPG" ALT="iMing" width="200" height="177.92869269949066213921901528014" BORDER="0">
+	   <br>
+	   <br><a href="../">About me</a>
+
+	   <br>
+	   <br><a href="../blog">Blog</a>
+<!--
+	   <br>
+	   <br><a href="../social">nikom2532</a>
+-->	
+	   <br>
+	   <br><a href="../_other/v2005/Index.htm">v2005 designed</a>
+	   <br>open with IE 5.5 or below
+	   <br>
+	   <br><a href="../_other/RSU/WebDesign/Index.htm">RSU designed contest</a>
+	   <br>open with IE 5.5 or below
+	   </center>
+	  </font>
+	 </td>
+	 <td valign="middle" style="width: 350px; " cellpadding="2" cellspacing="2">
+	  <font size="4" color="#ffffff">
+		<br>
+		<br>=================
+		<br>My Social Networking
+		<br>=================
+		<br>
+		<br><a href="https://www.ibm.com/developerworks/mydeveloperworks/profiles/user/ArmingHuang">IBM Developer works</a>
+		<br><a href="http://twitter.com/nikom2532">twitter: @nikom2532</a>
+		<br><a href="http://alturl.com/93nvt">Facebook</a>
+		<br><a href="https://plus.google.com/115185082420374327964/">Google plus</a>
+		<br><a href="http://www.skype.com">Skype name</a> : nikom2532
+		<br><a href="http://www.whatsapp.com">Whatapp</a> : (+xx)xx-xxxxxxx 
+		<br><a href="http://www.viber.com">Viber</a> : (+xx)xx-xxxxxxx
+		<br><a href="http://www.tango.me">Tango</a> : (+xx)xx-xxxxxxx
+		<br><a href="http://www.fring.com/">Fring</a> : (+xx)xx-xxxxxxx
+		<br><a href="mailto:nikom2532@hotmail.com">Windows Live Hotmail : nikom2532@hotmail.com</a>
+		<br><a href="http://www.imqq.com/">Tencent QQ</a> : 2379635310 <font color="#ff0000">[not avalable right now]</font>
+		<br><a href="mailto:1005892981@qq.com">QQ mail: 1005892981@qq.com</a> <font color="#ff0000">[not avalable right now]</font>
+		<br><a href="mailto:nikom2532@yahool.com">Yahoo Mail:  nikom2532@yahool.com</a>
+		<br><a href="mailto:nikom2532@gmail.com">Gmail: nikom2532@gmail.com</a>
+		<br><a href="http://soundcloud.com/nikom2532">SoundCloud</a>
+		<br><a href="http://t.qq.com/nikom2532">tweet.qq</a>
+		<br><a href="http://www.renren.com/profile.do?id=383146032">renren (人人网)</a>
+		<br><a href="http://www.spriiing.com">Spriiing</a> : 100BSS11
+		<br><a href="http://www.linkedin.com/in/nikom2532">LinkedIn</a>
+		<br><a href="http://www.hi5.com/friend/profile/displayProfile.do?userid=226392149">Hi5 </a>
+		<br><a href="http://pulse.yahoo.com/nikom2532">YAHOO! PULSE</a>
+		<br><a href="http://www.flickr.com/photos/nikom2532">Flickr
+		<br><a href="http://www.youtube.com/nikom2532">Youtube Channel</a>
+		<br><a href="http://picplz.com/user/nikom2532/">Picplz</a>
+		<br><a href="http://friendfeed.com/nikom2532">friendfeed</a>
+		<br><a href="http://www.blogger.com/profile/02508875033021847292">Blogger</a>
+		<br><a href="https://foursquare.com">Foursquare</a> : nikom2532
+		<br><a href="https://www.facebook.com/channel.v.bangkok.music">Channel [V] Bangkok Music</a>
+	  </font>
+	 </td>
+	</tr>
+   </table>
+
+   <br>
+   <P align="right">
+    <font color="#ddaa89">
+	viewed : 
+	<?php
+		include("./Counter/index.php"); 
+	?> 
+	<br>
+     Copyright(C) 2011, by Arming Huang (Nikom Suwankamol). Alright reserved (R)
+    </font>
+   </P>
+   <br><br>
+ </BODY>
+</HTML>
